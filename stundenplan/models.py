df814112eae6e789da4d661a2e0bf54285a2580b
Oliver Tietz <olitietz2007@gmail.com>
2025-01-19 16:39:33 +0100

    Klasse Lehrer erstellt

--- stundenplan/models.py
+++ stundenplan/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-class Name(models.Model):
+class Lehrer(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    short_name = models.CharField(max_length=3)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.first_name} {self.last_name} ({self.short_name})"
