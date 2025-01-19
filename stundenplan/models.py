c5e0d136090961093a47f2404b600ace133c7d73
Oliver Tietz <olitietz2007@gmail.com>
2025-01-19 16:53:27 +0100

    Klasse Class erstellt

--- stundenplan/models.py
+++ stundenplan/models.py
@@ -7,3 +7,11 @@ class Lehrer(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.short_name})"
+    
+class Class(models.Model):
+    name = models.CharField(max_length=10)
+    nr_of_students = models.IntegerField()
+    teachers = models.ManyToManyField(Lehrer)
+
+    def __str__(self):
+        return self.name
