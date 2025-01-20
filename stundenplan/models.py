b66104e85863d7eee1413a44584dc2872cc95862
Oliver Tietz <olitietz2007@gmail.com>
2025-01-20 11:22:46 +0100

    M2M-Relation Teacher und Class

--- stundenplan/models.py
+++ stundenplan/models.py
@@ -4,6 +4,7 @@ class Teacher(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     short_name = models.CharField(max_length=3)
+    pass
 
 class Grade(models.Model):
     grade = models.IntegerField()
@@ -11,8 +12,14 @@ class Grade(models.Model):
 class Class(models.Model):
     name = models.CharField(max_length=10)
     schueleranzahl = models.IntegerField()
+    teachers = models.ManyToManyField(Teacher, through='Teached_Subject')
 
 
 class Subject(models.Model):
     abkuerzung = models.CharField(max_length=10)
     name = models.CharField(max_length=100)
+
+class Teached_Subject(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    klasse = models.ForeignKey(Class, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100)
