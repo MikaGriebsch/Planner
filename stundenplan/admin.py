0048eb736b7b8783f94b9a9fed26375c981dd42a
Oliver Tietz <olitietz2007@gmail.com>
2025-01-20 10:34:29 +0100

    test

--- stundenplan/admin.py
+++ stundenplan/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
 from .models import Name
-from .models import Teacher
-from .models import Class
+
 
 admin.site.register(Name)
-admin.site.register(Teacher)
-admin.site.register(Class)
--- stundenplan/models.py
+++ stundenplan/models.py
@@ -4,23 +4,3 @@ class Name(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
-class Teacher(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    short_name = models.CharField(max_length=3)
-
-    def __str__(self):
-        return f"{self.first_name} {self.last_name} ({self.short_name})"
-    
-class Class(models.Model):
-    name = models.CharField(max_length=10)
-    nr_of_students = models.IntegerField()
-    teachers = models.ManyToManyField(Teacher, through="Teached_Subjects")
-
-    def __str__(self):
-        return self.name
-
-class Teached_Subjects(models.Model):
-    klasse = models.ForeignKey(Class, on_delete=models.CASCADE)
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
-    subject = models.CharField(max_length=100, default='')
