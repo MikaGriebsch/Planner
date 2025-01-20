819fba3db1b0f7af7170764281edf195acb1a048
Oliver Tietz <olitietz2007@gmail.com>
2025-01-20 10:59:13 +0100

    deleted model Name

--- stundenplan/admin.py
+++ stundenplan/admin.py
@@ -1,4 +1,1 @@
 from django.contrib import admin
-from .models import Name
-
-admin.site.register(Name)
--- stundenplan/views.py
+++ stundenplan/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from stundenplan.models import Name
+#from stundenplan.models import Name
 
 def index(request):
     #context = {
      #   'name': 'Mika',
       #  'class': '7A'
     #}
-    names = Name.objects.all()
-    return render(request, 'index.html', {'names': names})
+    #names = Name.objects.all()
+    #return render(request, 'index.html', {'names': names})
+    return render(request, 'index.html')
 
