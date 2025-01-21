ce15f7a4b34e124360484456038fe05e9c4e8a4d
Mika Griebsch <mika.griebsch@gmail.com>
2025-01-21 10:06:31 +0100

    Varibalen für Montag 1 Stunde

--- stundenplan/views.py
+++ stundenplan/views.py
@@ -3,7 +3,15 @@ from stundenplan.models import Subject
 
 def index(request):
   #Variablen in Stundenplan einfügen 
-  monA1 = "Hallo"
+  monA1 = "IF"
+  monA1Name = "Wf"
+  monA1Nr = "1.5"
   
   subjects = Subject.objects.all()
-  return render(request, 'index.html', {'subjects': subjects, 'monA1': monA1})
+  return render(request, 'index.html', 
+    {
+    'subjects': subjects, 
+    'monA1': monA1, 
+    'monA1Name': monA1Name, 
+    'monA1Nr': monA1Nr
+    })
