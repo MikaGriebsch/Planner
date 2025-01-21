900dcb2bdd9dee9d4ea64c42dda248f9269f8f63
Mika Griebsch <mika.griebsch@gmail.com>
2025-01-21 08:42:04 +0100

    Variablen in Stundenplan einfügen Kommentar

--- stundenplan/views.py
+++ stundenplan/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from stundenplan.models import Subject
 
 def index(request):
+  #Variablen in Stundenplan einfügen 
+  monA1 = "Hallo"
+  
   subjects = Subject.objects.all()
-  return render(request, 'index.html', {'subjects': subjects})
-
+  return render(request, 'index.html', {'subjects': subjects, 'monA1': monA1})
