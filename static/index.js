3eaba28c6e05c20400e70f58a86945c3cc6be2dc
Mika Griebsch <mika.griebsch@gmail.com>
2025-01-21 13:46:04 +0100

    simple input field to enter the grade you would like to see

--- static/index.js
+++ static/index.js
@@ -3,3 +3,9 @@ document.querySelector('.slider').addEventListener('click', () => {
     document.querySelector('#content').style.display = 'block';
 });
 
+
+function updateSpan() {
+    const input = document.getElementById('gradeInput').value;
+    const span = document.getElementById('dynamicText');
+    span.textContent = `Show time table from ${input}`;
+}
