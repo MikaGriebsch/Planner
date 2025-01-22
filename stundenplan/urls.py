82819254fa8ce9388f3f9b33b516a74ee9884faf
Mika Griebsch <mika.griebsch@gmail.com>
2025-01-22 13:43:23 +0100

    URL Abhänige anzeige auf der index

--- stundenplan/urls.py
+++ stundenplan/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('test/<str:klassenname>/', views.test_view, name='test_view'),
+    path('<str:klassenname>/', views.index_view, name='index_view'),
     
 ]
--- stundenplan/views.py
+++ stundenplan/views.py
@@ -2,15 +2,19 @@ from django.shortcuts import render
 from stundenplan.models import Subject
 from django.http import HttpResponse
 
-def index(request):
-  #Variablen in Stundenplan einfügen 
+
+def index_view(request, klassenname):
   monA1 = "IF"
   monA1Name = "Wf"
   monA1Nr = "1.5"
-  
+
   subjects = Subject.objects.all()
+
   return render(request, 'index.html', 
-    {
+  {
+    #class name
+    'klassenname': klassenname,
+    
     #first lesson
     'subjects': subjects, 
     'monA1': monA1, 
@@ -23,19 +27,4 @@ def index(request):
     #fifth/sixth lesson
     
     #seventh/eighth lesson
-    })
-
-def test_view(request, klassenname):
-    test_inhalte = {
-      "7a": "Dies ist die Seite für Klasse 7a.",
-      "7b": "Willkommen in Klasse 7b.",
-      "8a": "Hier findest du Infos zu Klasse 8a.",
-    }
-
-    # Überprüfen, ob die Klasse existiert
-    if klassenname in test_inhalte:
-      inhalt = test_inhalte[klassenname]
-    else:
-      inhalt = "Diese Klasse existiert nicht."
-
-    return HttpResponse(inhalt)
+  })
