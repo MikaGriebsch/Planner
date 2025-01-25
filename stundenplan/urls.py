c9ff3abc0a6e0ff9c1aeb7d2fd707de0d7289167
Jakob Leetz <jakob.leetz10@gmail.com>
2025-01-25 23:07:10 +0100

    name change

--- stundenplan/urls.py
+++ stundenplan/urls.py
@@ -6,7 +6,7 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('default/', views.default_view, name='default_view'),
-    path('planner/<str:klassenname>/', views.index_view, name='index_view'),
+    path('schedule/<str:klassenname>/', views.index_view, name='index_view'),
     path('login/', CustomLoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('register/', views.register, name='register'),
