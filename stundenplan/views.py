5e21b036f46ca80ba8dc8ce4636fcfd274b7b47c
mystsichBanane <pielothjulian@gmail.com>
2025-02-13 12:52:34 +0100

    Bearbeitung der Register Seite aka Create a new Password

--- stundenplan/views.py
+++ stundenplan/views.py
@@ -6,8 +6,12 @@ from .get_plan import get_plan
 
 @login_required
 def index_view(request, bezeichnung):
-    #usernamw
+    
     user = request.user
+    #if request.user.profile.firstLogin == True:
+    #    return render(request, 'register.html', {user}) #redirect to change password
+    #
+    #Das ist if und der Rets kommt bei Else
 
     if request.user.is_superuser or request.user.profile.klasse.bezeichnung == bezeichnung:
         context = get_plan(user, bezeichnung)
