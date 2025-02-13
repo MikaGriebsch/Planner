cc3e01edd876ff109fd6ed381411cc0154c0643e
Oliver Tietz <olitietz2007@gmail.com>
2025-02-13 18:32:25 +0100

    Halbjahresnoten können jetzt gespeichert werden

--- notentabelle/templatetags/noten_tags.py
+++ notentabelle/templatetags/noten_tags.py
@@ -6,3 +6,7 @@ register = template.Library()
 @register.filter
 def get_mark_for_subject(marks, subject):
     return marks.filter(subject=subject).first()
+
+@register.filter
+def get_semester_marks_for_subject(semester_marks, subject):
+    return semester_marks.filter(subject=subject).first()
