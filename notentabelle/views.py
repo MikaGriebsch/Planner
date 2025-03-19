c96354bbd11327548a5ad7938918180bb6dd8711
mystischBanane <pielothjulian@gmail.com>
2025-03-19 21:28:28 +0100

    SemesterMarks raus

--- notentabelle/views.py
+++ notentabelle/views.py
@@ -10,9 +10,9 @@ def index_view(request):
     user_class = request.user.profile.klasse
     subjects_in_class = Subject.objects.filter(grade=user_class.grade).distinct()
     marks = Mark.objects.filter(user=request.user, subject__in=subjects_in_class)
-    semester_marks = Semester_Marks.objects.filter(user=request.user, subject__in=subjects_in_class)
+    #semester_marks = Semester_Marks.objects.filter(user=request.user, subject__in=subjects_in_class)
 
-    return render(request, 'tabelle.html', {'marks': marks, 'subjects_in_class': subjects_in_class, 'semester_marks': semester_marks})
+    return render(request, 'tabelle.html', {'marks': marks, 'subjects_in_class': subjects_in_class,}) # 'semester_marks': semester_marks
 
 @require_POST
 def save_marks(request):
@@ -29,23 +29,23 @@ def save_marks(request):
             if field_name in request.POST:
                 value = request.POST[field_name]
                 setattr(mark, f"note_{i}", int(value) if value else None)
-
+        
         klausur_field_name = f"klausur_{mark.id}"
         if klausur_field_name in request.POST:
             value = request.POST[klausur_field_name]
             mark.klausur = int(value) if value else None
 
         mark.save()
+        #
+        #semester_mark = Semester_Marks.objects.filter(user=request.user, subject=subject).first()
+        #if not semester_mark:
+        #    semester_mark = Semester_Marks(user=request.user, subject=subject)
 
-        semester_mark = Semester_Marks.objects.filter(user=request.user, subject=subject).first()
-        if not semester_mark:
-            semester_mark = Semester_Marks(user=request.user, subject=subject)
+        #for i in range(1, 5):
+        #    field_name = f"semester_{i}_{semester_mark.id}"
+        #    if field_name in request.POST:
+        #        value = request.POST[field_name]
+        #        setattr(semester_mark, f"semester_{i}", int(value) if value else None)
 
-        for i in range(1, 5):
-            field_name = f"semester_{i}_{semester_mark.id}"
-            if field_name in request.POST:
-                value = request.POST[field_name]
-                setattr(semester_mark, f"semester_{i}", int(value) if value else None)
-
-        semester_mark.save()
+        #semester_mark.save()
     return redirect('index_view')
